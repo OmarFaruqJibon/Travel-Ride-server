d24551d11770569ca82b38f018792d8a73d3cbf2
MD Omar Faruq <omarfarukjibon418@gmail.com>
2021-10-31 10:41:53 +0600

    added port

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const ObjectId = require('mongodb').ObjectId;
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
@@ -56,14 +56,23 @@ async function run(){
         })
         // post user api
         app.post('/users', async(req,res)=>{
-            console.log('hitting user: ',req.body);
+            // console.log('hitting user: ',req.body);
             const users = req.body;
             const result = await usersCollections.insertOne(users);
             res.json(result);
         })
 
 
+        //DELETE API
+        app.delete('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            
+            const query = {_id: ObjectId(id)};
+            const result = await usersCollections.deleteOne(query);
 
+            console.log(query,result);
+            res.json(1);
+        })
 
 
 
